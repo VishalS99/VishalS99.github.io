bcd0e11daa746d8a0545f6f8d865075b2721d997
Vishal Saranathan <s-vishal1999@hotmail.com>
2023-09-22 12:24:32 -0400

    vercel deployment

--- backend/utils.py
+++ backend/utils.py
@@ -8,7 +8,7 @@ import boto3
 
 s3 = boto3.client(
     "s3",
-    aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+    aws_access_key_id=os.getenv('AWS_ACCESS_ID_KEY'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
 )
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
