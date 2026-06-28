6f179daff7ffd93612ab6dd4c517da04e6b4867b
Vishal Saranathan <s-vishal1999@hotmail.com>
2026-06-28 16:03:27 -0400

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+  base: "/",
   plugins: [solidPlugin()],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
-    outDir: 'build'
+    target: "esnext",
+    outDir: "build",
   },
 });
