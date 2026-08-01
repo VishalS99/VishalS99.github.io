8043eff91297aef737a82154839aeae6d22dc682
Vishal Saranathan <s-vishal1999@hotmail.com>
2026-08-01 13:31:41 -0400

    added responsive blob background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,15 +8,24 @@ import "./App.css";
 function App() {
   return (
     <div class="md:h-[calc(100vh-5.5rem)] md:border-none h-full border-2 border-primary ">
+      <picture class="block fixed inset-0 z-0 pointer-events-none w-full h-full opacity-40">
+        <source media="(min-width: 768px)" srcset="blob-scene-haikei.svg" />
+        <img
+          class="w-full h-full object-cover"
+          src="blob-scene-haikei_mobile.svg"
+          alt=""
+          aria-hidden="true"
+        />
+      </picture>
       <header class="sticky top-0 z-[100]">
         <Navbar />
       </header>
       {/* Mobile */}
-      <main class="md:hidden w-full flex flex-col">
+      <main class="relative z-10 md:hidden w-full flex flex-col">
         <MainBodyMobile />
       </main>
       {/* Desktop */}
-      <main class="hidden md:grid md:grid-cols-12 md:h-full md:mt-4">
+      <main class="relative z-10 hidden md:grid md:grid-cols-12 md:h-full md:mt-4">
         <div class="col-span-1">
           <SocialMedia />
         </div>
